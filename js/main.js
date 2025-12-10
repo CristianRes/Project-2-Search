bf15bf41d41c57a463b58efb88d0ff11ab359bd1
CristianRes <cristianrez07@gmail.com>
2025-12-10 12:15:35 -0600

    work on .js for gifs search

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 async function fetchData() {
     console.log (fetch ("https://api.giphy.com/v1/gifs/search?api_key=lGItLrwdRSuso1wZgkXewwrrgP7MDQtz&q=book&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"))
-    
-    
-    
     let responseObj = await fetch ("https://api.giphy.com/v1/gifs/search?api_key=lGItLrwdRSuso1wZgkXewwrrgP7MDQtz&q=book&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
     console.log(responseObj)
     console.log(responseObj.body)
 
-    let giphyjson =await responseObj.json()
+    let giphyjson = await responseObj.json()
     let giphyArray = giphyjson.data
     
 
     for (let i =0 ; i< giphyArray.length; i++) {
         console.log (giphyArray [i])
+        let imageGrab =  giphyArray [i]
+        console.log (imageGrab)
+        console.log(imageGrab.images)
+        console.log(imageGrab.images.original)
+        console.log(imageGrab.images.original.url)
+        let newImage = document.createElement("img")
+        newImage.src = imageGrab.images.original.url
+        document.getElementById("imageContainer").appendChild(newImage)
     }
+  
+}
 
-
-    }
 
 fetchData()
+
+
+// new function -> 
+// grab json image.fixed_width.url
