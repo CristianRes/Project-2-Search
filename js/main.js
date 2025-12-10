f75d333a8bb015c842d6af85a482626aa7ceee1b
CristianRes <cristianrez07@gmail.com>
2025-12-09 18:16:48 -0600

    working on .js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-const Select =
-    document.Navbar(
-        
-    )
+async function fetchData() {
+    console.log (fetch ("https://api.giphy.com/v1/gifs/search?api_key=lGItLrwdRSuso1wZgkXewwrrgP7MDQtz&q=book&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"))
+    
+    
+    
+    let responseObj = await fetch ("https://api.giphy.com/v1/gifs/search?api_key=lGItLrwdRSuso1wZgkXewwrrgP7MDQtz&q=book&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+    console.log(responseObj)
+    console.log(responseObj.body)
+
+    let giphyjson =await responseObj.json()
+    let giphyArray = giphyjson.data
+    
+
+    for (let i =0 ; i< giphyArray.length; i++) {
+        console.log (giphyArray [i])
+    }
+
+
+    }
+
+fetchData()
